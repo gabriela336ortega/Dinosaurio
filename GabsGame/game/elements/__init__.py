0f24d41c89105155eabdc3f5d5b3b98ed05105a5
Gabriela <gabriela336ortega@gmail.com>
2026-02-10 15:01:08 -0400

    a este elemento se le a importado el archivo de jumping_cactus.py

--- GabsGame/game/elements/__init__.py
+++ GabsGame/game/elements/__init__.py
@@ -3,3 +3,5 @@ from game.elements.dinosaur_element import Dinosaur
 from game.elements.large_cactus_element import LargeCactus
 from game.elements.small_cactus_element import SmallCactus
 from game.elements.pterodactyl_element import Bird
+from game.elements.jumping_cactus import JumpingCactus
+from game.elements.credist import show_credits
